be19c337788d163e507d6233506911cd42d607a8
Alldeales Ades <alldealesa@gmail.com>
2025-07-25 04:26:18 +0000

    components/nav-secondary.tsx "use client"
    import * as React from "react

--- src/components/nav-secondary.tsx
+++ src/components/nav-secondary.tsx
@@ -1,65 +1,42 @@
 "use client"
 
 import * as React from "react"
-import { type Icon as TablerIcon } from "@tabler/icons-react"
+import { type Icon } from "@tabler/icons-react"
+
 import {
+  SidebarGroup,
+  SidebarGroupContent,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from "@/components/ui/sidebar"
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip"
-import { cn } from "@/lib/utils"
 
-type NavSecondaryProps = React.ComponentProps<"div"> & {
+export function NavSecondary({
+  items,
+  ...props
+}: {
   items: {
     title: string
     url: string
-    icon: TablerIcon
-    isActive?: boolean
+    icon: Icon
   }[]
-}
-
-export function NavSecondary({ items, className, ...props }: NavSecondaryProps) {
-  const { isCollapsed } = useSidebar()
-
+} & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
   return (
-    <TooltipProvider>
-      <div className={cn("flex flex-col", className)} {...props}>
+    <SidebarGroup {...props}>
+      <SidebarGroupContent>
         <SidebarMenu>
-          {items.map((item, index) => {
-            const Icon = item.icon
-            return (
-              <SidebarMenuItem key={index} className="w-full">
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <SidebarMenuButton
-                      asChild
-                      isActive={item.isActive}
-                      className="w-full"
-                    >
-                      <a href={item.url}>
-                        <Icon className="size-5 shrink-0" />
-                        <span className="flex-1">{item.title}</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </TooltipTrigger>
-                  {isCollapsed && (
-                    <TooltipContent side="right" sideOffset={16}>
-                      {item.title}
-                    </TooltipContent>
-                  )}
-                </Tooltip>
-              </SidebarMenuItem>
-            )
-          })}
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
         </SidebarMenu>
-      </div>
-    </TooltipProvider>
+      </SidebarGroupContent>
+    </SidebarGroup>
   )
 }
